5843f7ee18a13048dd6eb5c1152d2709f0cae8ba
Bruna <bferreira.slv@gmail.com>
2022-12-12 17:44:44 -0300

    Ajuste de tipo de retorno da conversão

--- app/process/converte.py
+++ app/process/converte.py
@@ -1,16 +1,25 @@
 def conversao(dados):
-    convertido = []
+    # convertido = []
+    convertido = ''
+
     for i in dados:
         if i == 'Sempre':
-            convertido.append(3)
+            convertido += '3'
+            # convertido.append('3')
         elif i == 'Quase Sempre':
-            convertido.append(2)
+            # convertido.append('2')
+            convertido += '2'
         elif i == 'Raramente':
-            convertido.append(1)
+            # convertido.append('1')
+            convertido += '1'
         elif i == 'Nunca':
-            convertido.append(0)
+            # convertido.append('0')
+            convertido += '0'
         elif i == 'Masculino':
-            convertido.append(0)
+            # convertido.append('0')
+            convertido += '0'
         elif i == 'Feminino':
-            convertido.append(1)
+            # convertido.append('1')
+            convertido += '1'
+
     return convertido
